b715bcb99f0b1e0718107ea0573ae57c82cc7fe5
Chlopes92 <christelle.lopes92@gmail.com>
2022-12-15 15:46:58 +0100

    Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { JournalComponent } from './pages/journal/journal.component';
+import { CgvComponent } from './pages/cgv/cgv.component';
+import { PolitiqueComponent } from './pages/politique/politique.component';
+import { MentionComponent } from './pages/mention/mention.component';
+import { PanierComponent } from './pages/panier/panier.component';
+import { AchatComponent } from './pages/achat/achat.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProposComponent } from './pages/propos/propos.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    CategoryComponent,
+    JournalComponent,
+    CgvComponent,
+    PolitiqueComponent,
+    MentionComponent,
+    PanierComponent,
+    AchatComponent,
+    ContactComponent,
+    ProposComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
